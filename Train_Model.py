c5cd2f8f5633ac1d0ddc58a7375e4005be8d186a
dungpv133 <vnphan5.1@gmail.com>
2024-08-04 22:45:15 +0700

    fix resnet custom'

--- Train_Model.py
+++ Train_Model.py
@@ -33,8 +33,8 @@ def prepara_data():
     train_ds, val_ds = random_split(myds, [num_train, num_val])
 
     # Create training and validation data loaders
-    train_dl = torch.utils.data.DataLoader(train_ds, batch_size=8, shuffle=True)
-    val_dl = torch.utils.data.DataLoader(val_ds, batch_size=8, shuffle=False)
+    train_dl = torch.utils.data.DataLoader(train_ds, batch_size=32, shuffle=True)
+    val_dl = torch.utils.data.DataLoader(val_ds, batch_size=32, shuffle=False)
 
     return (train_dl, val_dl)
 
