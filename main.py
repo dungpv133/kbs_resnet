166bffc21e371e705c3a6954a9b3f0c7549cd0e2
dungpv133 <vnphan5.1@gmail.com>
2024-08-05 13:50:14 +0700

    fix saving ckpt

--- main.py
+++ main.py
@@ -182,7 +182,7 @@ if __name__ == '__main__':
         model_inf = nn.DataParallel(ResNetCustom(num_classes = 2))
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model_inf = model_inf.to(device)
-        model_inf.load_state_dict(torch.load('/kaggle/working/kbs_resnet/model_command.pt'))
+        model_inf.load_state_dict(torch.load('/kaggle/input/kbs-clean/model_command.pt'))
         model_inf.eval()
 
         # Perform inference
